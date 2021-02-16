b114f7e8aef10d557e9111fe820b771850a0639d
emirxmertoglu <emirxmertoglu@outlook.com>
2021-02-16 22:16:03 +0300

    added postlist and singlepost component for home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts" />
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import PostList from "../components/PostList";
 export default {
-  name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title:
+          "2021 Sony Dünya Fotoğraf Ödülleri'nde Türkiye Ulusal Ödülü'nün Sahibi Belli Oldu",
+        body:
+          "Sony Türkiye, Dünya Fotoğraf Organizasyonu ile beraber 2021 Sony Dünya Fotoğraf Ödülleri Türkiye Ulusal Ödülü’nü Mehmet Aslan’ın kazandığını açıkladı.",
+        id: 1,
+      },
+      {
+        title:
+          "Microsoft, Şubat Ayının İkinci Yarısı İçin Xbox Game Pass Oyunlarını Duyurdu",
+        body:
+          "Xbox Game Pass kullanıcılarının ücretsiz oynayabileceği içeriklere yenileri ekleniyor. 18 Şubat’tan itibaren Game Pass’e gelecek oyunlar PC ve konsol ortamında yayınlanacak.",
+        id: 2,
+      },
+    ]);
+    return { posts };
+  },
 };
 </script>
+
+<style>
+</style>
